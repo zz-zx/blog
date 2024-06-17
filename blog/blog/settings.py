42fd415d9aaf7db0b25bf804f7a8f828cab5328c
张梦瑶“ <3110196420@qq.com>
2024-06-17 21:03:02 +0800

    '图片验证码'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -196,3 +196,6 @@ LOGGING = {
         },
     }
 }
+#替换系统的User 来使用我们自己 定义的User
+#配置信息为‘子应用名.模型类型’
+AUTH_USER_MODEL = 'users.User'
--- blog/users/urls.py
+++ blog/users/urls.py
@@ -1,10 +1,12 @@
 #进行users子应用的视图路由
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView,ImageCodeView
 
 urlpatterns = [
     # 参数1：路由
     # 参数2：视图函数
     # 参数3：路由名，方便通过reverse来获取路由
     path('register/',RegisterView.as_view(),name='register'),
+    #图片验证码的路由
+    path('imagecode/',ImageCodeView.as_view(),name='imagecode'),
 ]
